18b5e0aacc0e0a362d3664b73b717d0846559708
Holt Skinner <HoltWasHere@gmail.com>
2018-03-03 22:37:39 -0600

    Added Criterion Function

--- pcm.py
+++ pcm.py
@@ -2,7 +2,24 @@ import numpy as np
 import skfuzzy as fuzz
 
 
-def pcm(x, c, m=2, e=0.01, max_iterations=100, v=None):
+def eta():
+    return 1
+
+
+def criterion_function(x, v, eta, m):
+    d = np.square(np.linalg.norm(x - v))
+
+    ex = 2 / (m - 1)
+    fz = np.power(d / eta, ex)
+    u = 1 / (1 + fz)
+    return u
+
+
+def update_cluster_centers():
+    return
+
+
+def pcm(x, c, m=2, e=0.01, max_iterations=100, v0=None):
     """
     Possibilistic C-Means Algorithm
 
@@ -24,12 +41,13 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v=None):
     `max_iterations` int, optional  
         Maximum number of iterations
 
-    `v` array-like, optional  
+    `v0` array-like, optional  
         Initial cluster centers
 
     ### Returns
 
-    `cluster_centers` 2D Array, size (S, c)
+    `v` 2D Array, size (S, c)  
+        Cluster centers
 
     `u` 2D Array (S, N)  
         Final partitioned matrix
@@ -40,7 +58,7 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v=None):
     `d` 2D Array (S, N)  
         Distance Matrix
     
-    `p` int  
+    `t` int  
         Number of iterations run
 
     `f` float  
@@ -61,14 +79,37 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v=None):
 
     # Initialize the cluster centers
     # If the user doesn't provide their own starting points,
-    if not v or not v.any():
+    if not v0 or not v0.any():
         # Pick random values from dataset
-        v = np.random.choice(x, c, True)
+        v0 = np.random.choice(x, c, True)
     else:
         # Turn the input array into a numpy array
-        v = np.array(v)
+        v0 = np.array(v0)
 
-    return
+    # List of all cluster centers (Bookkeeping)
+    v = np.array([v0])
+
+    # Partition Matrix
+    u = np.array([[[]]])
+
+    n = eta()
+
+    # Number of Iterations
+    t = 0
+
+    while t <= max_iterations:
+
+        # Cycle through x's
+        for k in range(len(x)):
+            for i in range(len(v[t])):
+                u[t, i, k] = criterion_function(x[k], v[i], n, m)
+
+        t += 1
+        # Stopping Criteria
+        if np.linalg.norm(v[t] - v[t - 1]) < e:
+            break
+
+    return v
 
 
 data = np.random.randint(100, size=5)
