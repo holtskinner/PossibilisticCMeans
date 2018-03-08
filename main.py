d0cddadeac3edb2a87ba95d7ef0914f2d7641a2d
Holt Skinner <HoltWasHere@gmail.com>
2018-03-08 17:13:06 -0600

    Fixed some of the dimensionality issues

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import skfuzzy as fuzzy
 from plot import plot
 from pcm import pcm
 
-num_datapoints = 1000
+num_datapoints = 3
 num_features = 2
 
 x = np.random.uniform(low=0, high=1000, size=(num_datapoints, num_features))
@@ -13,6 +13,6 @@ y = np.random.uniform(low=2000, high=3000, size=(num_datapoints, num_features))
 xy = np.concatenate((x, y), axis=0)
 np.random.shuffle(xy)
 
-plot(xy)
+# plot(xy)
 
 y = pcm(x=xy, c=2, m=2)
--- pcm.py
+++ pcm.py
@@ -17,10 +17,8 @@ def criterion_function(x, v, eta, m):
 
 def update_cluster_centers(x, c, m, u):
 
-    n = len(x[0])
-
-    v = np.zeros((c, n))
-
+    # v = np.zeros((c, ))
+    # TODO Fix Dimensionality
     for i in range(c):
         numerator = 0
         denominator = 0
@@ -30,6 +28,10 @@ def update_cluster_centers(x, c, m, u):
             numerator += fuzzified * x[k]
             denominator += fuzzified
 
+        print(numerator)
+
+        print(denominator)
+        print("\n")
         v[i] = numerator / denominator
 
     return v
@@ -41,7 +43,7 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v0=None):
 
     ### Parameters
 
-    `x` 2D array, size (S, N)  
+    `x` 2D array, size (N, S)  
         Data to be clustered. N is the number of data sets;
         S is the number of features within each sample vector. 
 
@@ -82,9 +84,12 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v0=None):
 
     """
 
-    if not x.any():
-        print("Error: Data cannot be None")
+    if not x.any() or len(x) < 1 or len(x[0]) < 1:
+        print("Error: Data is in incorrect format")
         return
+
+    N = len(x)  # Number of datapoints
+    S = len(x[0])  # Number of features
 
     if not c or c <= 0:
         print("Error: Number of clusters must be at least 1")
@@ -95,30 +100,29 @@ def pcm(x, c, m=2, e=0.01, max_iterations=100, v0=None):
 
     # Initialize the cluster centers
     # If the user doesn't provide their own starting points,
-    if not v0 or not v0.any():
+    if not v0 or not v0.any() or len(v0) != c:
         # Pick random values from dataset
-        v0 = np.random.choice(x, c, True)
-    else:
-        # Turn the input array into a numpy array
-        v0 = np.array(v0)
+        v0 = x[np.random.choice(x.shape[0], c, replace=True), :]
 
     # List of all cluster centers (Bookkeeping)
-    v = np.array([v0])
+    v = np.zeros((max_iterations, c, S))
+    v[0] = np.array(v0)
 
-    # Membership Matrix
-    u = np.array([[[]]])
+    # Membership Matrix Each Data Point in eah cluster
+    u = np.zeros((max_iterations, N, c))
 
     n = eta()
 
     # Number of Iterations
     t = 0
 
-    while t <= max_iterations:
+    while t < max_iterations - 1:
 
-        # Cycle through x's
-        for k in range(len(x)):
-            for i in range(len(v[t])):
-                u[t, i, k] = criterion_function(x[k], v[i], n, m)
+        # Cycle through datapoints's
+        for k in range(N):
+            # cycle through clusters
+            for i in range(c):
+                u[t, k, i] = criterion_function(x[k], v[t, i], n, m)
 
         t += 1
 
