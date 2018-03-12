182a6e4e314fd32a3fd044e109e43a3e9233442e
Holt Skinner <HoltWasHere@gmail.com>
2018-03-11 20:41:45 -0500

    Simplified data generator

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 import numpy as np
 import sklearn as sk
+from sklearn.datasets import make_blobs
 import skfuzzy as fuzz
 from plot import plot
 import cmeans
 
-num_datapoints = 1000
+num_samples = 100000
 num_features = 2
+c = 6
+fuzzifier = 2
+error = 0.001
+maxiter = 100
 
-# Define three cluster centers
-centers = np.array([[10, 10], [0, 0], [5, 5]])
+x = make_blobs(num_samples, num_features, c)[0].T
 
-# Define three cluster sigmas in x and y, respectively
-sigmas = np.array([[0.8, 0.3], [0.3, 0.5], [1.1, 0.7]])
+v, u, u0, d, t, f = cmeans.fcm(x, c, fuzzifier, error, maxiter)
 
-# Generate test data
-# np.random.seed(42)  # Set seed for reproducibility
-x = np.zeros(1)
-y = np.zeros(1)
+plot(x, v, u, c)
 
-for i, ((xmu, ymu), (xsigma, ysigma)) in enumerate(zip(centers, sigmas)):
-    x = np.hstack((x,
-                   np.random.standard_normal(num_datapoints) * xsigma + xmu))
-    y = np.hstack((y,
-                   np.random.standard_normal(num_datapoints) * ysigma + ymu))
+v, u, u0, d, t, f = cmeans.pcm(x, c, fuzzifier, error, maxiter, v0=v)
 
-xy = np.vstack((x, y))
-
-v, u, u0, d, _, t, f = fuzz.cmeans(xy, 3, 5, 0.0001, 100)
-
-v, u, u0, d, t, f = cmeans.pcm(
-    x=xy, c=3, m=2, e=0.001, max_iterations=100, metric="euclidean", v0=v)
-
-plot(xy, v, u, 3)
+plot(x, v, u, c)
--- plot.py
+++ plot.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 def plot(xy, v, u, c):
 
     fig, ax = plt.subplots()
-    colors = ["#cc79a7", "#0072b2", "#d55e00", "#009e73"]
 
     # Plot assigned clusters, for each data point in training set
     cluster_membership = np.argmax(u, axis=0)
@@ -17,7 +16,7 @@ def plot(xy, v, u, c):
             label=f"Class {j}",
             alpha=0.5,
             edgecolors="none",
-            color=colors[j])
+            c=np.random.random(3))
 
     # Mark the center of each fuzzy cluster
     for pt in v:
