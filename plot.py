cebf2dbc66d2c3b7496b05f1257cfc4147f974dd
Holt Skinner <holt@umc-143127.dhcp.missouri.edu>
2018-04-29 11:38:35 -0500

    Added PCA Decomposition to plot

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
 
 
 def plot(x, v, u, c, labels=None):
@@ -9,16 +10,14 @@ def plot(x, v, u, c, labels=None):
     # Plot assigned clusters, for each data point in training set
     cluster_membership = np.argmax(u, axis=0)
 
+    x = PCA(n_components=2).fit_transform(x).T
+
     for j in range(c):
         ax.scatter(
             x[0][cluster_membership == j],
             x[1][cluster_membership == j],
             alpha=0.5,
             edgecolors="none")
-
-    # Mark the center of each fuzzy cluster
-    for pt in v:
-        ax.plot(pt[0], pt[1], 'rs')
 
     ax.legend()
     ax.grid(True)
