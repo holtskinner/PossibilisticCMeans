3b4b81cdc4531e5840c496398edd8ac07c0edd41
Holt Skinner <holt@umc-143127.dhcp.missouri.edu>
2018-04-29 11:46:57 -0500

    Added Cluster Validty measure

--- main.py
+++ main.py
@@ -5,32 +5,69 @@ import skfuzzy as fuzz
 from plot import plot
 import cmeans
 
-num_samples = 500
+
+def generate_data(num_samples, num_features, c, shuffle=True):
+
+    x = ds.make_blobs(num_samples, num_features, c, shuffle=False)[0]
+
+    x = x.T
+
+    labels = np.zeros(num_samples)
+    labels[0:100] = 0
+    labels[100:200]
+
+    j = num_samples / c
+
+    for i in range(c):
+        labels[(i * j):((i + 1) * j)] = i
+
+    return x, labels
+
+
+def verify_clusters(x, c, v, u, labels):
+
+    ssd_actual = 0
+
+    for i in range(c):
+        # All points in class
+        x1 = x[labels == i]
+        # Mean of class
+        m = np.mean(x1, axis=0)
+
+        for pt in x1:
+            ssd_actual += np.linalg.norm(pt - m)
+
+    clm = np.argmax(u, axis=0)
+    ssd_clusters = 0
+
+    for i in range(c):
+        # Points clustered in a class
+        x2 = x[clm == i]
+
+        for pt in x2:
+            ssd_clusters += np.linalg.norm(pt - v[i])
+
+    print(ssd_clusters / ssd_actual)
+
+
+num_samples = 300000
 num_features = 2
-c = 4
+c = 3
 fuzzifier = 1.2
-error = 0.0001
+error = 0.001
 maxiter = 100
 
 # np.random.seed(100)
 
-x = ds.make_blobs(num_samples, num_features, c)[0]
+x, labels = generate_data(num_samples, num_features, c, shuffle=False)
 
-# np.random.shuffle(x)
+v, v0, u, u0, d, t = cmeans.fcm(x, c, fuzzifier, error, maxiter)
 
-x = x.T
+plot(x.T, v, u, c)
 
-# v, v0, u, u0, d, t = cmeans.fcm(x, c, fuzzifier, error, maxiter)
+print("Blobs")
+verify_clusters(x.T, c, v, u, labels)
 
-# plot(x, v, u, c)
-
-v, v0, u, u0, d, t = cmeans.hcm(x, c, error, maxiter)
-
-print(t)
-
-plot(x, v, u, c)
-
-'''
 iris = ds.load_iris()
 
 labels = iris.target_names
@@ -38,8 +75,38 @@ target = iris.target
 iris = np.array(iris.data).T
 
 c = 3
-v, v0, u, u0, d, t = cmeans.fcm(iris, c, fuzzifier, error, maxiter)
 
-print(v0)
-plot(iris, v, u, c, labels)
-'''
+v, v0, u, u0, d, t = cmeans.fcm(iris, c, fuzzifier, error, maxiter)
+iris = iris.T
+
+print("Iris")
+
+verify_clusters(iris, c, v, u, target)
+
+
+digits = ds.load_digits()
+
+labels = digits.target
+
+digits = np.array(digits.data).T
+
+c = 10
+
+v, v0, u, u0, d, t = cmeans.pcm(digits, c, fuzzifier, error, maxiter)
+
+print("Digits")
+verify_clusters(digits.T, c, v, u, labels)
+# plot(digits.T, v, u, c)
+
+c = 2
+cancer = ds.load_breast_cancer()
+
+labels = cancer.target
+
+cancer = np.array(cancer.data).T
+v, v0, u, u0, d, t = cmeans.fcm(cancer, c, fuzzifier, error, maxiter)
+
+plot(cancer.T, v, u, c)
+
+print("Cancer")
+verify_clusters(cancer.T, c, v, u, labels)
